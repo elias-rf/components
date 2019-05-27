b5c6b277da595536207d75443d3a4af210dc73ea
Elias Ribeiro <eliasribeiro@gmail.com>
2019-05-27 17:34:28 -0300

    ok

---
+++ src/generateError.ts
@@ -0,0 +1,19 @@
+import httpStatus from "@erf/utils/dist/httpStatus";
+
+interface APIError extends Error {
+  status?: string | number;
+  statusText?: string;
+}
+
+/**
+ * Return new validation error
+ *
+ * @param {Error} errorData
+ * @returns {Error|APIError}
+ */
+export default function generateError(message, status) {
+  const err: APIError = new Error(message);
+  err.status = status;
+  err.statusText = httpStatus[status];
+  return err;
+}
