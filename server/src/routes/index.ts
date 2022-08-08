09f954181336dbdd31bbe3a70199e35aba654363
Elias Ribeiro <53453802+elias-rf@users.noreply.github.com>
2022-08-08 15:15:29 -0300

    fix server.routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,10 +2,10 @@ import Router from "@koa/router";
 import { routesCreate } from "../../../utils";
 
 // import knex from "./knex.router";
-// import auth from "./auth.router";
+import lixo from "./lixo.router";
 import { rpcRouter } from "./rpc.router";
 
-const api = [...rpcRouter];
+const api = [...rpcRouter, ...lixo];
 
 export const router = new Router({ prefix: "/api" });
 routesCreate(router, api);
--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    watch: true,
+    watch: false,
     outputTruncateLength: 200,
     snapshotFormat: { escapeString: false },
   },
