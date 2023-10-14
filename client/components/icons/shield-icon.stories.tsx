2efb0b729c0e7a3f3b25f2f2e71dbfac06565740
Elias Ribeiro <53453802+elias-rf@users.noreply.github.com>
2023-10-14 13:54:58 -0300

    add client/shield-icon

---
+++ client/components/icons/shield-icon.stories.tsx
@@ -0,0 +1,8 @@
+import { ShieldIcon } from '@/client/components/icons/shield-icon'
+import type { Story, StoryDefault } from '@ladle/react'
+
+export default {
+  title: 'Components / Icons / Shield Icon',
+} satisfies StoryDefault
+
+export const Default: Story = () => <ShieldIcon />
