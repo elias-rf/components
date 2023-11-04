97a9f8fc3a1c3362012f2407f937b9cba1678cee
Elias Ribeiro <53453802+elias-rf@users.noreply.github.com>
2023-11-03 21:15:24 -0300

    clear not used code

--- utils/array/difference.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Retorna os elementos de array1 que não aparecem em array2
- */
-
-export function difference(array1: any[], array2: any[]) {
-  const arrays = [array1, array2];
-  return arrays.reduce((a, b) => a.filter((c) => !b.includes(c)));
-}
