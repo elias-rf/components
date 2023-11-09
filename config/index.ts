57a50c4f62a4575d4cf6d07173ed58ad53af1916
Elias Ribeiro <53453802+elias-rf@users.noreply.github.com>
2023-11-09 11:09:09 -0300

    fix config

--- config/index.ts
+++ config/index.ts
@@ -1,95 +1,69 @@
-import dotenv from "dotenv-flow";
-import type { Knex } from "knex";
-import tls from "tls";
+import dotenv from 'dotenv-flow'
+import type { Knex } from 'knex'
+import tls from 'tls'
 
 dotenv.config({
   silent: true,
-});
+})
 
-tls.DEFAULT_MIN_VERSION = "TLSv1";
+tls.DEFAULT_MIN_VERSION = 'TLSv1'
+
+type TSqlConfig = {
+  client: string
+  debug: boolean
+  useNullAsDefault: boolean
+  connection: {
+    database: string
+    host: string
+    user: string
+    password: string
+    options: {
+      trustServerCertificate: boolean
+      enableArithAbort: boolean
+      tdsVersion?: string
+    }
+  }
+}
 
 interface Config {
   app: {
-    port: number;
-    env: string;
-    mock?: boolean;
-  };
+    port: number
+    env: string
+    mock?: boolean
+  }
   db: {
-    sys: Knex.Config;
-    oftalmo: Knex.Config;
-    plano: Knex.Config;
-    fullvision: Knex.Config;
-  };
+    sys: Knex.Config
+    oftalmo: TSqlConfig
+    plano: TSqlConfig
+    fullvision: TSqlConfig
+  }
   auth: {
-    expiration: number;
-    secret: string;
-  };
+    expiration: number
+    secret: string
+  }
 }
 
 export const config: Config = {
   app: {
-    port: parseInt(process.env.PORT ? process.env.PORT : "3000"),
-    env: process.env.NODE_ENV || "development",
-    mock: process.env.MOCK === "true",
+    port: parseInt(process.env.PORT ? process.env.PORT : '3000'),
+    env: process.env.NODE_ENV || 'development',
+    mock: process.env.MOCK === 'true',
   },
   auth: {
-    expiration: parseInt(process.env.auth_expiration || "3600"),
-    secret: process.env.auth_secret || "S3gr3d0",
+    expiration: parseInt(process.env.auth_expiration || '3600'),
+    secret: process.env.auth_secret || 'S3gr3d0',
   },
   db: {
     sys: {
-      client: "better-sqlite3",
+      client: 'better-sqlite3',
       debug: false,
       useNullAsDefault: true,
       connection: {
-        filename: "./sys.sqlite",
+        filename: './sys.sqlite',
       },
     },
-    oftalmo: {
-      client: "mssql",
-      debug: false,
-      useNullAsDefault: true,
-      connection: {
-        database: process.env.db_oftalmo_connection_database,
-        host: process.env.db_oftalmo_connection_host,
-        user: process.env.db_oftalmo_connection_user,
-        password: process.env.db_oftalmo_connection_password,
-        options: {
-          trustServerCertificate: true,
-          enableArithAbort: true,
-          tdsVersion: "7_3_B",
-        },
-      },
-    },
-    plano: {
-      client: "mssql",
-      debug: false,
-      useNullAsDefault: true,
-      connection: {
-        database: process.env.db_plano_connection_database,
-        host: process.env.db_plano_connection_host,
-        user: process.env.db_plano_connection_user,
-        password: process.env.db_plano_connection_password,
-        options: {
-          enableArithAbort: true,
-          trustServerCertificate: true,
-        },
-      },
-    },
-    fullvision: {
-      client: "mssql",
-      debug: false,
-      useNullAsDefault: true,
-      connection: {
-        database: process.env.db_fullvision_connection_database,
-        host: process.env.db_fullvision_connection_host,
-        user: process.env.db_fullvision_connection_user,
-        password: process.env.db_fullvision_connection_password,
-        options: {
-          enableArithAbort: true,
-          trustServerCertificate: true,
-        },
-      },
-    },
+    oftalmo: JSON.parse(process.env.db_oftalmo || ''),
+    plano: JSON.parse(process.env.db_plano || ''),
+    fullvision: JSON.parse(process.env.db_fullvision || ''),
   },
-};
+}
--- server/index.ts
+++ server/index.ts
@@ -4,16 +4,23 @@
  * @description usa fastify, trpc
  */
 
-import { config } from "@/config";
-import { app } from "./app";
+import { config } from '@/config'
+import { app } from './app'
 
 const start = async () => {
   try {
-    await app.listen({ host: "0.0.0.0", port: config.app.port });
+    await app.listen({ host: '0.0.0.0', port: config.app.port })
+    console.log({
+      db: {
+        fullvision: config.db.fullvision.connection.host,
+        oftalmo: config.db.oftalmo.connection.host,
+        plano: config.db.plano.connection.host,
+      },
+    })
   } catch (err) {
-    console.log(err);
-    process.exit(1);
+    console.log(err)
+    process.exit(1)
   }
-};
+}
 
-start();
+start()
